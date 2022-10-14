a5d4571da38e131f66b26f1a9e5a2f236530a549
melih.kacaman <melih.kacaman@stu.fsm.edu.tr>
2022-10-14 22:54:02 +0300

    ilk kümeleme kmeans modeli tamamlandı.

---
+++ common/model_helper.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+
+
+def get_embeddings(model: tf.keras.Model, data):
+    """
+    model: the model that's has been built with tf.keras.Model
+    data: data that preprocessed with tf.keras.preprocessing.image_dataset_from_directory function
+
+    :return: embeddings, images, labels
+
+    """
+
+    return None
