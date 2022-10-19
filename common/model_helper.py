36acc6d703b7aa6f54f9022c9201f3359afb9873
melih.kacaman <melih.kacaman@stu.fsm.edu.tr>
2022-10-19 12:02:23 +0300

    üg cinsiyet detayında classifier calismasi icin verinin cekilmesi

--- common/model_helper.py
+++ common/model_helper.py
@@ -1,6 +1,9 @@
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
+import cv2
+import requests
+import os
 
 
 def get_embeddings(model: tf.keras.Model, data):
@@ -21,3 +24,69 @@ def show_images(idxs, images, row=1):
         plt.subplot(row, 3, i + 1)
         plt.axis('off')
         plt.imshow(np.array(images[idxs[i]], np.int32))
+
+
+def save_image_cv(url, scale_percent, path, default_size=False):
+    resp = requests.get(url, stream=True).raw
+    image = np.asarray(bytearray(resp.read()), dtype="uint8")
+    if image is None:
+        return
+    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+
+    width = 224
+    height = 224
+
+    if default_size is False:
+        width = int(image.shape[1] * scale_percent / 100)
+        height = int(image.shape[0] * scale_percent / 100)
+
+    dsize = (width, height)
+    output = cv2.resize(image, dsize)
+
+    cv2.imwrite(path, output)
+
+
+def make_dataset_cv(bran_id, dataset_name, datasource, class_names, iteration=None):
+    # RENK range is removed. 19-10-2022
+    problems_of_paths = []
+    try:
+        base = os.path.abspath(os.path.join(os.path.dirname(os.getcwd()), '.'))
+        path = os.path.join('datasets', dataset_name)
+        path = os.path.join(base, path)
+        if not os.path.isdir(path):
+            os.mkdir(path)
+        path = os.path.join(path, f'brand_{bran_id}')
+        if not os.path.isdir(path):
+            os.mkdir(path)
+        # datasets/dataset/brand_id
+
+        iterator = 0
+        for index, row in class_names.iterrows():
+            _class = row['CinsiyetKodu'] + '-' + row['UrunGrubuKodu']
+            folder = os.path.join(path, _class)
+            res = datasource.query(f'MarkaKodu == "{bran_id}" and CinsiyetKodu == "{row.CinsiyetKodu}" and '
+                                   f'UrunGrubuKodu == "{row.UrunGrubuKodu}"')
+
+            # if exists any data
+            if res.shape[0] > 0:
+                # check the folder exist
+                if not os.path.isdir(folder):
+                    os.mkdir(folder)
+
+                # add the imgs to the folder
+                for img_index, img_row in res.iterrows():
+                    try:
+                        save_image_cv(img_row['cUrl'], 50, os.path.join(folder, str(img_index) + '.png'), True)
+                        if iteration is not None and iterator == iteration:
+                            break
+                        else:
+                            iterator += 1
+                    except Exception as e2:
+                        problems_of_paths.append(os.path.join(folder, str(img_index) + '.jpg'))
+                        print(e2)
+                        continue
+
+    except Exception as e:
+        print('An exception occurred.', e)
+
+    return problems_of_paths
--- queries/backbone_ds_queries.sql
+++ queries/backbone_ds_queries.sql
@@ -3,7 +3,9 @@
 	Backbone Dataset - For Similar Products 
 	Created By Melih Kaçaman 
 
-	Son bir yýl içerisinde performansý en yüksek olan 10 ÜG'den 1000 seçenek. 
+	Son bir yýl içerisinde performansý en yüksek olan 
+	
+	10 ÜG'den 1000 seçenek. 
 */ 
 
 declare @today date = convert(date, getdate()) 
@@ -12,6 +14,8 @@ declare @lastOneYear date = dateadd(year,-1, @today)
 -- Ürün Grubu performansý hesaplama 
 
 -- USPA Cinsiyet UG E-Ticarette satýþ sayýsý 
+-- Belki maðazada almayý tercih ettiðim þeyi eticarette almayý daha az tercih edebilirim.  
+
 SELECT 
 	M.SidMalzemeCinsiyet,
 	M.SidMalzemeUrunGrubu, 
@@ -42,53 +46,134 @@ ORDER BY
 	2 DESC 
 	
 -- UG Performansý
-SELECT top 10 
+SELECT 
 	UG.SidMalzemeCinsiyet,
 	UG.SidMalzemeUrunGrubu,
 	cast(UG.UGSayisi / (C.CSayisi * 1.0) as decimal(15, 3)) UGPerformans 
 INTO 
-	 #UGPerformans 
+	 #UGPerformans -- drop table #UGPerformans
 FROM 
 	#UGSatis UG INNER JOIN 
 	#CSatis C ON UG.SidMalzemeCinsiyet = C.SidMalzemeCinsiyet
 ORDER BY 
 	UGPerformans DESC 
 
--- her iki 5 li UG'de kendi cinsiyetinin satýþlarýnýn %50 sini oluþturuyor. 
 
--- belirlenen ürün gruplarýna ait seçenekler altýnda seçenek ve renk performansý hesaplama 
+select SidMalzemeCinsiyet, count(distinct SidMalzemeUrunGrubu) say from #UGPerformans group by SidMalzemeCinsiyet
+
+select * into #SecilenUG from (
+		select * from #UGPerformans WHERE SidMalzemeCinsiyet = 3 
+		UNION 
+		SELECT TOP 42 * FROM #UGPerformans WHERE SidMalzemeCinsiyet = 4 ORDER BY UGPerformans DESC
+	) K 
+
+
+
+
 
 SELECT 
-	S.Tarih,
-	M.SidMalzeme, 
-	M.SidMalzemeCinsiyet, 
-	M.SidMalzemeUrunGrubu, 
-	M.SidSecenek, 
-	M.Renk 
+	M.WebSecenek, 
+	MM.Kodu MarkaKodu, 
+	MC.Kodu CinsiyetKodu, 
+	MUG.UrunGrubu UrunGrubuKodu, 
+	M.Renk,
+	M.ResimAdresi ResimUrl 
 INTO 
-	#SalesData 
+	#SecenekResim 
 FROM 
-	MIX.dbo.vSatis S INNER JOIN 
-	MIX.dim.vMalzeme M ON M.SidMalzeme = S.SidMalzeme INNER JOIN 
-	#UGPerformans UGP ON UGP.SidMalzemeCinsiyet = M.SidMalzemeCinsiyet AND UGP.SidMalzemeUrunGrubu = M.SidMalzemeUrunGrubu 
+	MIX.dim.vMalzeme M 
+	INNER JOIN (SELECT DISTINCT SidMalzemeCinsiyet, SidMalzemeUrunGrubu FROM #SecilenUG) GSUG 
+		ON GSUG.SidMalzemeCinsiyet = M.SidMalzemeCinsiyet AND GSUG.SidMalzemeUrunGrubu = M.SidMalzemeUrunGrubu 
+	INNER JOIN MIX.dim.vMalzemeMarka MM on MM.SidMalzemeMarka = M.SidMalzemeMarka
+	INNER JOIN MIX.dim.vMalzemeCinsiyet MC on MC.SidMalzemeCinsiyet = M.SidMalzemeCinsiyet
+	INNER JOIN MIX.dim.vMalzemeUrunGrubu MUG on MUG.SidMalzemeUrunGrubu = M.SidMalzemeUrunGrubu
 WHERE 
-	S.SidSatisKanali = 11 AND 
-	S.Tarih > @lastOneYear AND 
-	M.SidMalzemeMarka = 4
-	
--- marka -> cinsiyet -> ürün grubu -> seçenek -> malzeme 
+	M.ResimAdresi is not null and M.ResimAdresi not like '%tedarik%' AND 
+	M.SidMalzemeMarka = 4 
+GROUP BY 
+	M.WebSecenek, 
+	MM.Kodu, 
+	MC.Kodu, 
+	MUG.UrunGrubu, 
+	M.Renk,
+	M.ResimAdresi;  
 
-SELECT  
-	S.SidMalzemeCinsiyet, 
-	S.SidMalzemeUrunGrubu, 
-	S.SidSecenek, 
-	CAST(COUNT(S.SidSecenek) / (S2.UGSatisSayisi * 1.0) AS decimal(15, 3)) SecenekPerformans 
+
+INSERT INTO 
+	#SecenekResim
+SELECT 
+	*
+FROM
+	#SecenekResim 
+
+
+SELECT 
+	K.WebSecenek, 
+	K.MarkaKodu, 
+	K.CinsiyetKodu, 
+	K.UrunGrubuKodu, 
+	K.Renk, 
+	CASE 
+		WHEN K.UrlNo % 2 != 0 THEN K.ResimUrl
+		ELSE REPLACE(K.ResimUrl, '-1.jpg', '-2.jpg')
+	END cURL, 
+	K.UrlNo 
+INTO 
+	#Dataset2 
 FROM 
-	#SalesData S INNER JOIN 
-	(SELECT SidMalzemeCinsiyet, SidMalzemeUrunGrubu, count(SidSecenek) UGSatisSayisi FROM #SalesData GROUP BY SidMalzemeCinsiyet, SidMalzemeUrunGrubu) S2 ON S2.SidMalzemeCinsiyet = S.SidMalzemeCinsiyet AND S2.SidMalzemeUrunGrubu = S.SidMalzemeUrunGrubu
-GROUP BY	
-	S.SidMalzemeCinsiyet, S.SidMalzemeUrunGrubu, SidSecenek, s2.UGSatisSayisi
-ORDER BY 
-	SecenekPerformans DESC 
+	(SELECT 
+		S.*,  
+		ROW_NUMBER() OVER(PARTITION BY S.WebSecenek ORDER BY S.ResimUrl) UrlNo
+	FROM 
+		#SecenekResim S) K 
 
 
+CREATE TABLE PROTO.Melih.Dataset2_SimilarProducts(
+	WebSecenek varchar(50), --41 
+	MarkaKodu varchar(50), 
+	CinsiyetKodu nvarchar(50), 
+	UrunGrubuKodu varchar(50), 
+	Renk varchar(20), 
+	cUrl varchar(200),
+	UrlNo int 
+)
+-- truncate table PROTO.Melih.Dataset2_SimilarProducts 
+-- drop table PROTO.Melih.Dataset2_SimilarProducts 
+-- INSERT INTO PROTO.Melih.Dataset2_SimilarProducts 
+
+select DISTINCT  
+	C.Kodu,
+	C.CinsiyetKisaAdi,
+	UG.UrunGrubu, 
+	count(*) as classnum 
+INTO 
+	#prep2 -- drop table #prep2 
+from #Dataset2 S 
+INNER JOIN MIX.dim.vMalzemeCinsiyet C on C.Kodu = s.CinsiyetKodu 
+INNER JOIN MIX.dim.vMalzemeUrunGrubu UG ON UG.UrunGrubu = S.UrunGrubuKodu 
+WHERE S.MarkaKodu = '08' 
+group by C.Kodu, C.CinsiyetKisaAdi, UG.UrunGrubu
+having count(*) > 500 -- bu sayýnýn doðru tespit edilmesi gerek. 
+order by classnum desc  
+
+
+
+-- drop table #prep3 
+select S.* INTO #prep3 from #Dataset2 s INNER JOIN #prep2 P on P.Kodu = S.CinsiyetKodu and P.UrunGrubu = S.UrunGrubuKodu 
+where s.MarkaKodu = '08' 
+
+
+select p.CinsiyetKodu, p.UrunGrubuKodu, count(WebSecenek) as seceneksay from #prep3 p
+group by p.CinsiyetKodu, p.UrunGrubuKodu
+order by seceneksay desc  
+
+
+select CinsiyetKodu, count(UrunGrubuKodu) from #prep3 group by CinsiyetKodu 
+
+-- dataset2 125 bin img içeriyor. 
+-- bir classta en az 526 fotoðraf var. 
+-- bir classta en fazla 10170 fotoðraf var. 
+-- bu durum modelin kurulum esnasýnda deðerlendirilecek. 
+
+INSERT INTO PROTO.Melih.Dataset2_SimilarProducts 
+SELECT * FROM #prep3
