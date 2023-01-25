7cebd9425925d75f545069508dedd12b126b4870
Melih Kaçaman <melih.kacaman@stu.fsm.edu.tr>
2023-01-25 11:26:37 +0300

    show image method is coded for evaluation

--- notebooks/helpers.py
+++ notebooks/helpers.py
@@ -1,22 +1,6 @@
 import pandas as pd 
 import tensorflow as tf 
-
-def to_csv(dataframe: pd.DataFrame, path:str): 
-    """
-        It save the dataframe into the folder which path points. 
-        Also, it will add the dtypes as a record as a last row. 
-    """ 
-    dataframe.loc[-1] = dataframe.dtypes 
-    dataframe.index = dataframe.index + 1 
-    dataframe.sort_index(inplace=True) 
-    dataframe.to_csv(path, index=False) 
-
-def read_csv_with_dtypes(path:str):
-    """
-        CSV file has to have its dtypes at the last row. 
-    """
-    dtypes = pd.read_csv(path, nrows=1).iloc[0].to_dict() 
-    return pd.read_csv(path, dtype=dtypes, skiprows=[1]) 
+import matplotlib.pyplot as plt 
 
 
 def evaluate_img(path: str, model: tf.keras.models.Model, columns: list, dims: tuple = (224, 224, 3), threshold: int = 0.5):
@@ -41,3 +25,29 @@ def evaluate_img(path: str, model: tf.keras.models.Model, columns: list, dims: t
     result.index = columns 
 
     return result
+
+def show_image(path, width=224, height=224): 
+    img = tf.io.read_file(path) 
+    img = tf.image.decode_jpeg(img, channels=3)
+    img = tf.image.resize(img, [width, height], antialias=False) 
+    img = img / tf.constant([255], dtype=tf.float32) 
+
+    plt.imshow(img) 
+    plt.show()
+
+def to_csv(dataframe: pd.DataFrame, path:str): 
+    """
+        It save the dataframe into the folder which path points. 
+        Also, it will add the dtypes as a record as a last row. 
+    """ 
+    dataframe.loc[-1] = dataframe.dtypes 
+    dataframe.index = dataframe.index + 1 
+    dataframe.sort_index(inplace=True) 
+    dataframe.to_csv(path, index=False) 
+
+def read_csv_with_dtypes(path:str):
+    """
+        CSV file has to have its dtypes at the last row. 
+    """
+    dtypes = pd.read_csv(path, nrows=1).iloc[0].to_dict() 
+    return pd.read_csv(path, dtype=dtypes, skiprows=[1]) 
